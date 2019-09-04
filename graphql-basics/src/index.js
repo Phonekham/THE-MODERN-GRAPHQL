6d6faf914cff8f82d210114dc95b438d3d2ce600
Phonekham <phone.madridista@gmail.com>
2019-09-04 15:13:54 +0700

    setup spread operator

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -162,9 +162,7 @@ const resolvers = {
       }
       const user = {
         id: uuidv4(),
-        name: args.name,
-        email: args.email,
-        age: args.age
+        ...args
       };
       users.push(user);
       return user;
@@ -176,10 +174,7 @@ const resolvers = {
       }
       const post = {
         id: uuidv4(),
-        title: args.title,
-        body: args.body,
-        published: args.published,
-        author: args.author
+        ...args
       };
       posts.push(post);
       return post;
@@ -194,9 +189,7 @@ const resolvers = {
       }
       const comment = {
         id: uuidv4(),
-        text: args.text,
-        author: args.author,
-        post: args.post
+        ...args
       };
       comments.push(comment);
       return comment;

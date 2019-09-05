c56d61b4d2ba3cec490f08047002ff093be55f9f
Phonekham <phone.madridista@gmail.com>
2019-09-05 15:52:15 +0700

    Setting up a Comments Subscription

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -88,7 +88,7 @@ const Mutation = {
     }
     return post;
   },
-  createComment(parent, args, { db }, info) {
+  createComment(parent, args, { db, pubsub }, info) {
     const userExist = db.users.some(user => user.id === args.data.author);
     const postExist = db.posts.some(
       post => post.id === args.data.post && post.published
@@ -101,6 +101,7 @@ const Mutation = {
       ...args.data
     };
     db.comments.push(comment);
+    pubsub.publish(`comment ${args.data.post}`, { comment });
     return comment;
   },
   deleteComment(parent, args, { db }, info) {

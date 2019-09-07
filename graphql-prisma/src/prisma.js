70837f1cb47887518fe67e11cff01bc9d10f44b7
Phonekham <phone.madridista@gmail.com>
2019-09-07 20:50:50 +0700

    Using prisma binding

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -1,6 +1,19 @@
 import { Prisma } from "prisma-binding";
+import { stringify } from "querystring";
 
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
   endpoint: "http://localhost:4466"
 });
+
+const user = prisma.query
+  .users(null, "{id name posts {id title} }")
+  .then(data => {
+    console.log(JSON.stringify(data, undefined, 2));
+  });
+
+prisma.query
+  .comments(null, "{id text author {id name} post {id title}}")
+  .then(data => {
+    console.log(JSON.stringify(data, undefined, 2));
+  });

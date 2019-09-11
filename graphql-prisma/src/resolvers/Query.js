cb528d6b64386169701037bf7010493cefdfafc6
Phonekham <phone.madridista@gmail.com>
2019-09-11 17:32:33 +0700

    Sorting Data

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -5,7 +5,8 @@ const Query = {
     const opArgs = {
       first: args.first,
       skip: args.skip,
-      after: args.after
+      after: args.after,
+      orderBy: args.orderBy
     };
     if (args.query) {
       opArgs.where = {
@@ -24,6 +25,7 @@ const Query = {
       first: args.first,
       skip: args.skip,
       after: args.after,
+      orderBy: args.orderBy,
       where: {
         author: {
           id: userId
@@ -47,6 +49,7 @@ const Query = {
       first: args.first,
       skip: args.skip,
       after: args.after,
+      orderBy: args.orderBy,
       where: {
         published: true
       }
@@ -69,7 +72,8 @@ const Query = {
     const opArgs = {
       first: args.first,
       skip: args.skip,
-      after: args.after
+      after: args.after,
+      orderBy: args.orderBy
     };
     return prisma.query.comments(opArgs, info);
   },

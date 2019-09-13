ba791b0c108d6ff4bcf4020021c3851eddec5d6d
Phonekham <phone.madridista@gmail.com>
2019-09-13 21:50:04 +0700

    4. Testing and Assertions

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,3 +1,25 @@
-test("My first test case", () => {
-  1 + 2;
+import { getFirstName, isValidPassword } from "../src/utils/user";
+
+test("Should return first name", () => {
+  const firstName = getFirstName("Phonekham Keomany");
+  expect(firstName).toBe("Phonekham");
+});
+test("Should return first name when given firsname", () => {
+  const firstName = getFirstName("Toh");
+  expect(firstName).toBe("Toh");
+});
+
+test("Should reject if password less than 8", () => {
+  const isValid = isValidPassword("abc123");
+  expect(isValid).toBe(false);
+});
+
+test("Should reject if password contain password", () => {
+  const isValid = isValidPassword("abcPassword343");
+  expect(isValid).toBe(false);
+});
+
+test("Should correct valid password", () => {
+  const isValid = isValidPassword("abc123wewewewe");
+  expect(isValid).toBe(true);
 });

2b69f0e2e860ab17ed13a73ceadf75f167d02790
Phonekham <phone.madridista@gmail.com>
2019-09-16 16:50:35 +0700

    10. Testing Queries

--- tests/user.test.js
+++ tests/user.test.js
@@ -8,18 +8,31 @@ const client = new ApolloBoost({
 });
 
 beforeEach(async () => {
+  jest.setTimeout(780000);
   await prisma.mutation.deleteManyPosts();
   await prisma.mutation.deleteManyUsers();
   const user = await prisma.mutation.createUser({
     data: {
-      name: "name",
+      name: "phonekham",
       email: "email@gmail.com",
       password: bcrypt.hashSync("Phone@1234")
     }
   });
   await prisma.mutation.createPost({
     data: {
-      title: "my title",
+      title: "My published post",
+      body: "",
+      published: true,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  });
+  await prisma.mutation.createPost({
+    data: {
+      title: "My draft post",
       body: "",
       published: false,
       author: {
@@ -52,4 +65,37 @@ test("Should create a new user", async () => {
     id: response.data.createUser.user.id
   });
   expect(exists).toBe(true);
+});
+
+test("Should expose public author profile", async () => {
+  const getUsers = gql`
+    query {
+      users {
+        id
+        name
+        email
+      }
+    }
+  `;
+  const response = await client.query({ query: getUsers });
+  expect(response.data.users.length).toBe(1);
+  expect(response.data.users[0].email).toBe(null);
+  expect(response.data.users[0].name).toBe("phonekham");
+});
+
+test("Should expose published posts", async () => {
+  const getPosts = gql`
+    query {
+      posts {
+        id
+        title
+        body
+        published
+      }
+    }
+  `;
+  const response = await client.query({ query: getPosts });
+
+  expect(response.data.posts.length).toBe(1);
+  expect(response.data.posts[0].published).toBe(true);
 });

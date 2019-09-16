520dcc9fd8c5135e5de8fa86ed6b984c1dea7162
Phonekham <phone.madridista@gmail.com>
2019-09-16 15:26:35 +0700

    9. Seeding the Database with Test Data

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,9 +1,34 @@
 import "cross-fetch/polyfill";
 import ApolloBoost, { gql } from "apollo-boost";
+import bcrypt from "bcryptjs";
 import prisma from "../src/prisma";
 
 const client = new ApolloBoost({
   uri: "http://localhost:4000"
+});
+
+beforeEach(async () => {
+  await prisma.mutation.deleteManyPosts();
+  await prisma.mutation.deleteManyUsers();
+  const user = await prisma.mutation.createUser({
+    data: {
+      name: "name",
+      email: "email@gmail.com",
+      password: bcrypt.hashSync("Phone@1234")
+    }
+  });
+  await prisma.mutation.createPost({
+    data: {
+      title: "my title",
+      body: "",
+      published: false,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  });
 });
 
 test("Should create a new user", async () => {
